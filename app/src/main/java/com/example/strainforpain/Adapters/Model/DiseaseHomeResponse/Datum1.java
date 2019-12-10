2bfaef79092534c6b8d1049bdeee534dfdf785a7
axiftaj <47206155+axiftaj@users.noreply.github.com>
2019-12-10 16:12:32 +0500

    new version

---
+++ app/src/main/java/com/example/strainforpain/Adapters/Model/DiseaseHomeResponse/Datum1.java
@@ -0,0 +1,112 @@
+
+package com.example.strainforpain.Adapters.Model.DiseaseHomeResponse;
+
+
+import com.google.gson.annotations.SerializedName;
+
+
+@SuppressWarnings("unused")
+public class Datum1 {
+
+    @SerializedName("CBC")
+    private String mCBC;
+    @SerializedName("CBD")
+    private String mCBD;
+    @SerializedName("CBG")
+    private String mCBG;
+    @SerializedName("CBN")
+    private String mCBN;
+    @SerializedName("disease_id")
+    private String mDiseaseId;
+    @SerializedName("id")
+    private String mId;
+    @SerializedName("image")
+    private String mImage;
+    @SerializedName("THC")
+    private String mTHC;
+    @SerializedName("THCV")
+    private String mTHCV;
+    @SerializedName("title")
+    private String mTitle;
+
+    public String getCBC() {
+        return mCBC;
+    }
+
+    public void setCBC(String cBC) {
+        mCBC = cBC;
+    }
+
+    public String getCBD() {
+        return mCBD;
+    }
+
+    public void setCBD(String cBD) {
+        mCBD = cBD;
+    }
+
+    public String getCBG() {
+        return mCBG;
+    }
+
+    public void setCBG(String cBG) {
+        mCBG = cBG;
+    }
+
+    public String getCBN() {
+        return mCBN;
+    }
+
+    public void setCBN(String cBN) {
+        mCBN = cBN;
+    }
+
+    public String getDiseaseId() {
+        return mDiseaseId;
+    }
+
+    public void setDiseaseId(String diseaseId) {
+        mDiseaseId = diseaseId;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getImage() {
+        return mImage;
+    }
+
+    public void setImage(String image) {
+        mImage = image;
+    }
+
+    public String getTHC() {
+        return mTHC;
+    }
+
+    public void setTHC(String tHC) {
+        mTHC = tHC;
+    }
+
+    public String getTHCV() {
+        return mTHCV;
+    }
+
+    public void setTHCV(String tHCV) {
+        mTHCV = tHCV;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+}
---
+++ app/src/main/java/com/example/strainforpain/models/DetailsModel/Datum2.java
@@ -0,0 +1,142 @@
+
+package com.example.strainforpain.models.DetailsModel;
+
+
+import com.google.gson.annotations.SerializedName;
+
+
+@SuppressWarnings("unused")
+public class Datum2 {
+
+    @SerializedName("CBC")
+    private String mCBC;
+    @SerializedName("CBD")
+    private String mCBD;
+    @SerializedName("CBG")
+    private String mCBG;
+    @SerializedName("CBN")
+    private String mCBN;
+    @SerializedName("Description")
+    private String mDescription;
+    @SerializedName("image")
+    private String mImage;
+    @SerializedName("Origins")
+    private String mOrigins;
+    @SerializedName("Potency")
+    private String mPotency;
+    @SerializedName("THC")
+    private String mTHC;
+    @SerializedName("THCV")
+    private String mTHCV;
+    @SerializedName("title")
+    private String mTitle;
+    @SerializedName("Type")
+    private String mType;
+    @SerializedName("Url")
+    private String mUrl;
+
+    public String getCBC() {
+        return mCBC;
+    }
+
+    public void setCBC(String cBC) {
+        mCBC = cBC;
+    }
+
+    public String getCBD() {
+        return mCBD;
+    }
+
+    public void setCBD(String cBD) {
+        mCBD = cBD;
+    }
+
+    public String getCBG() {
+        return mCBG;
+    }
+
+    public void setCBG(String cBG) {
+        mCBG = cBG;
+    }
+
+    public String getCBN() {
+        return mCBN;
+    }
+
+    public void setCBN(String cBN) {
+        mCBN = cBN;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public String getImage() {
+        return mImage;
+    }
+
+    public void setImage(String image) {
+        mImage = image;
+    }
+
+    public String getOrigins() {
+        return mOrigins;
+    }
+
+    public void setOrigins(String origins) {
+        mOrigins = origins;
+    }
+
+    public String getPotency() {
+        return mPotency;
+    }
+
+    public void setPotency(String potency) {
+        mPotency = potency;
+    }
+
+    public String getTHC() {
+        return mTHC;
+    }
+
+    public void setTHC(String tHC) {
+        mTHC = tHC;
+    }
+
+    public String getTHCV() {
+        return mTHCV;
+    }
+
+    public void setTHCV(String tHCV) {
+        mTHCV = tHCV;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getType() {
+        return mType;
+    }
+
+    public void setType(String type) {
+        mType = type;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+}
